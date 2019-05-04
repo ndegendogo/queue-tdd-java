72fc1c6f55e1d95292b3e966523bce10205f3b6c
ndegendogo <christa.runge@kifaru.de>
2019-05-05 00:58:45 +0200

    Refactor production code.

--- src/main/java/Queue.java
+++ src/main/java/Queue.java
@@ -1,48 +1,45 @@
 import static java.lang.System.arraycopy;
 
 public class Queue {
-    private int size;
     private int[] elements = new int[0];
     private int pushIndex;
     private int popIndex;
 
     public boolean isEmpty() {
-        return size == 0;
+        return getSize() == 0;
+    }
+
+    public boolean isFull() {
+        return getSize() == elements.length;
     }
 
     public void add(int i) {
         if (isFull())
             grow();
-        int index = pushIndex % elements.length;
-        elements[index] = i;
-        pushIndex = pushIndex + 1;
-        size++;
-    }
-
-    private boolean isFull() {
-        return size == elements.length;
+        elements[pushIndex++ % elements.length] = i;
     }
 
     public int remove() {
         if (isEmpty())
             throw new Underflow();
-        int result = elements[popIndex];
-        popIndex = (popIndex + 1) % elements.length;
-        size--;
-        return result;
+        return elements[popIndex++ % elements.length];
     }
 
     private void grow() {
+        int size = getSize();
         int[] newElements = new int[size + 1];
-        arraycopy(elements, popIndex, newElements, 0, size - popIndex);
-        arraycopy(elements, 0, newElements, size - popIndex, popIndex);
+        if (!isEmpty()) {
+            popIndex = popIndex % elements.length;
+            arraycopy(elements, popIndex, newElements, 0, size - popIndex);
+            arraycopy(elements, 0, newElements, size - popIndex, popIndex);
+        }
         elements = newElements;
         popIndex = 0;
         pushIndex = size;
     }
 
     public int getSize() {
-        return size;
+        return pushIndex - popIndex;
     }
 
     int getMemory() {
